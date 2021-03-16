610934d99c2d73364daea8631a659435839d2cb6
knagara <nagara.keita@gmail.com>
2021-03-16 23:08:24 +0900

    Form

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,46 @@
     <keep-alive>
       <component :is="currentComponent"></component>
     </keep-alive>
+
+    <!--フォーム-->
+    <div>
+      <h2>フォーム</h2>
+      <label for="title">タイトル</label>
+      <input id="title" type="text" v-model="eventData.title">
+      <input type="text" v-model.lazy="eventData.title">
+      <p>{{eventData.title}}</p>
+      <label for="maxNumber">最大人数</label>
+      <input id="maxNumber" type="number" v-model.number="eventData.number">
+      <p>{{eventData.number}}</p>
+      <label for="host">主催者</label>
+      <input id="host" type="text" v-model.trim="eventData.host">
+      <p>{{eventData.host}}</p>
+      <label for="detail">イベントの内容</label>
+      <textarea id="detail" cols="30" rows="10" v-model="eventData.detail"></textarea>
+      <pre>{{eventData.detail}}</pre>
+      <p style="white-space: pre;">{{eventData.detail}}</p>
+      <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
+      <label for="isPrivate">非公開</label>
+      <p>{{eventData.isPrivate}}</p>
+      <p>参加条件</p>
+      <input type="checkbox" id="10" value="10代" v-model="eventData.target">
+      <label for="10">10代</label>
+      <input type="checkbox" id="20" value="20代" v-model="eventData.target">
+      <label for="10">20代</label>
+      <input type="checkbox" id="30" value="30代" v-model="eventData.target">
+      <label for="10">30代</label>
+      <p>{{eventData.target}}</p>
+      <p>参加費</p>
+      <input type="radio" id="free" value="無料" v-model="eventData.price">
+      <label for="free">無料</label>
+      <input type="radio" id="payed" value="有料" v-model="eventData.price">
+      <label for="payed">有料</label>
+      <p>開催地</p>
+      <select v-model="eventData.location">
+        <option v-for="location in locations" :key="location">{{location}}</option>
+      </select>
+      <p>{{eventData.location}}</p>
+    </div>
   </div>
 </template>
 
@@ -43,7 +83,18 @@ export default {
   data(){
     return {
       number: 10,
-      currentComponent: 'Home'
+      currentComponent: 'Home',
+      locations: ['東京','大阪','名古屋'],
+      eventData: {
+        title: "",
+        maxNumber: 0,
+        host: "",
+        detail: "",
+        isPrivate: false,
+        target: [],
+        price: '無料',
+        location: '東京'
+      }
     }
   },
   components:{
